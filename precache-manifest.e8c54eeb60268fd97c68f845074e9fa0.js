51bc11c32f3a6fa7701e68432bbfa64318a1db7e
Minh Tran <minh.lootting@gmail.com>
2020-07-20 18:58:09 -0400

    Updates

---
+++ precache-manifest.e8c54eeb60268fd97c68f845074e9fa0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d12333f54d24b123be5fe6d46d3a9cf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "590c673e09845a192d85",
+    "url": "/static/css/main.60f0aaa9.chunk.css"
+  },
+  {
+    "revision": "ed5182d9c9ddb2be65fc",
+    "url": "/static/js/2.c2d68d13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2d68d13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "590c673e09845a192d85",
+    "url": "/static/js/main.44e545fd.chunk.js"
+  },
+  {
+    "revision": "7232379b937d4fc75088",
+    "url": "/static/js/runtime-main.26d5b08d.js"
+  },
+  {
+    "revision": "3fa4ea4934f864ced7702864739609b4",
+    "url": "/static/media/pic1.3fa4ea49.jpg"
+  },
+  {
+    "revision": "92e6e1b547641e388e21e0e797f74654",
+    "url": "/static/media/pic2.92e6e1b5.jpg"
+  }
+]);
