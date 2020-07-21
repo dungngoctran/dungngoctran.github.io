a234bdcf08a2bdd5012827c1f7a27f24c9008954
Minh Tran <minh.lootting@gmail.com>
2020-07-21 15:11:56 -0400

    more pictures and fixed header

--- src/Gallery.js
+++ src/Gallery.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './Gallery.css';
 import Pic1 from './resources/pic1.jpg';
 import Pic2 from './resources/pic2.jpg';
+import Pic3 from './resources/pic3.jpg';
+import Pic4 from './resources/pic4.jpg';
+import Pic5 from './resources/pic5.jpg';
 
 class Gallery extends React.Component {
 
@@ -10,6 +13,9 @@ class Gallery extends React.Component {
 			<div className="Gallery">
 				<img src={Pic1} alt="Day 1 angle 1"/>
 				<img src={Pic2} alt="Day 1 angle 2"/>
+				<img src={Pic3} alt="Day 2 pic 1"/>
+				<img src={Pic4} alt="Day 2 pic 2"/>
+				<img src={Pic5} alt="Day 2 pic 3"/>
 			</div>
 		)
 	}
